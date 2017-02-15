050ac6a2f8fd0f9f403d1402b0a0e52da3d227bb
Dwt <FineexMac@FineEX-LFdeMac-mini.local>
2017-02-15 09:24:41 +0800

    创建分类

---
+++ WTImageEasyInOut/WTImageEasyInOut/UIImageView+EasyInOut.h
@@ -0,0 +1,13 @@
+//
+//  UIImageView+EasyInOut.h
+//  WTImageEasyInOut
+//
+//  Created by Dwt on 2017/2/15.
+//  Copyright © 2017年 Dwt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImageView (EasyInOut)
+
+@end
---
+++ WTImageEasyInOut/WTImageEasyInOut/UIImageView+EasyInOut.m
@@ -0,0 +1,13 @@
+//
+//  UIImageView+EasyInOut.m
+//  WTImageEasyInOut
+//
+//  Created by Dwt on 2017/2/15.
+//  Copyright © 2017年 Dwt. All rights reserved.
+//
+
+#import "UIImageView+EasyInOut.h"
+
+@implementation UIImageView (EasyInOut)
+
+@end
