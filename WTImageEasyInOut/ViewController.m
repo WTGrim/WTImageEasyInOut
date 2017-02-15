7a76ec70d4273c52c48fd480542a454727faf791
Dwt <FineexMac@FineEX-LFdeMac-mini.local>
2017-02-15 09:19:30 +0800

    初始化工作

---
+++ WTImageEasyInOut/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WTImageEasyInOut
+//
+//  Created by Dwt on 2017/2/15.
+//  Copyright © 2017年 Dwt. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
